0db4f66ad904cc54838e92013232d4bc9a843d4e
Sanawar_21 <sanawarsaeed@gmail.com>
2024-01-18 00:14:49 +0500

    add further api compatibilty

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@
 from cog import BasePredictor, Input, Path
 from app import generate
 from src.utils import paths, restore_dirs, make_archive
+from src.client import download_zip
 import shutil
 import os
 
@@ -14,23 +15,30 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
-        input_video: Path = Input(description="Input video"),
+        session_id: str = Input(
+            default=None, description="Required if input video and audio not provided"),
+        input_video: Path = Input(
+            default=None, description="Input video (required if session ID not provided)"),
         input_audio: Path = Input(
-            description="Input audio (wav only) for voice cloning (optional)", default=None),
-        voice_name: str = Input(description="Voice name"),
+            description="Input audio (wav only) for voice cloning (optional; can also be provided via the session ID)", default=None),
+        voice_name: str = Input(description="Voice name (required)"),
         description: str = Input(
             description="Voice description (optional)", default=None),
-        video_topic: str = Input(description="Video topic"),
-        content_type: str = Input(description="Type of content"),
-        key_points: str = Input(description="Key points"),
+        video_topic: str = Input(description="Video topic (required)"),
+        content_type: str = Input(description="Type of content (required)"),
+        key_points: str = Input(description="Key points (required)"),
     ) -> Path:
         """Run a single prediction on the model"""
 
         restore_dirs()
 
-        shutil.copy(input_video, paths.input_video)
-        if input_audio is not None:
-            shutil.copy(input_audio, paths.input_audio)
+        if session_id == None:
+            shutil.copy(input_video, paths.input_video)
+            if input_audio is not None:
+                shutil.copy(input_audio, paths.input_audio)
+        else:
+            download_zip(session_id)
+
         workflow = {
             'VideoTopic': video_topic,
             'TypeOfContent': content_type,
